2f4bab3977edb144d1357e462557269976b75b2b
Laurent <lbuffevant@gmail.com>
2011-12-21 11:13:26 +0100

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+S3SwfTest::Application.config.secret_token = '4fba1585f35c73565962e011eb96f871dd9cc5f2638fa20a6fd05931b4832bd2bb47fff10047d8c50c7e11275ef746569249e2a0967aa899fcb2b268085dbd38'
