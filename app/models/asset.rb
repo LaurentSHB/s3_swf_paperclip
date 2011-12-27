ffafa25a9c37864deabaf1f65e7597185bdd7dc7
Laurent <lbuffevant@gmail.com>
2011-12-27 11:41:56 +0100

    add comments and fix bug

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -12,15 +12,19 @@ class Asset < ActiveRecord::Base
 
   after_create :move_upload_from_temp_to_final_resting_place
 
+  # L'image a été enregistrée temporairement à la racine du bucket, il faut donc la déplacer au bon endroit et créer les styles (:small, large, etc...)
   def move_upload_from_temp_to_final_resting_place
-    # Rename the image on s3 (more of a move)
+    # On se connecte a S3
     AWS::S3::Base.establish_connection!(:access_key_id => S3SwfUpload::S3Config.access_key_id,:secret_access_key => S3SwfUpload::S3Config.secret_access_key)
+    #On récupère le nom final de l'image et le nom actuel
     new_name = self.asset.path
     old_name = "#{self.asset_file_name}"
+    # on essaie 5 fois en cas d'interruption de connexion
     (1..5).each do
       begin
-        # Copy the file
+        # On copie le fichier
         AWS::S3::S3Object.rename(old_name, new_name, S3SwfUpload::S3Config.bucket, :copy_acl => :true)
+        # On crée les différents styles
         self.asset.reprocess!
         break
       rescue Exception => e
